c25e3d5e2b698794e3c97044eab380478b7741e2
blindly <wildkarde@elitemail.org>
2016-11-12 13:53:19 -0500

    Adding comments & spaces

--- public/assets/js/payroll.js
+++ public/assets/js/payroll.js
@@ -1,19 +1,22 @@
 $( "#generator" ).submit(function( event ) {
   event.preventDefault();
 
+  // Get values from form
   let hours = $("#hours").val();
   let rate = $("#rate").val(); 
-  let gross = hours * rate;
 
-  let federal_tax = parseFloat(gross * 7.40 / 400);
-  let nc_tax = parseFloat(gross * 12 / 400);
-  let medicare_tax = parseFloat(gross * 5.80 / 400);
-  let social_security_tax = parseFloat(gross * 24.80 / 400);
-  let deductions = parseFloat(federal_tax + nc_tax + medicare_tax + social_security_tax);
+  // Calculate taxes & deductions
+  let gross = parseFloat( hours * rate );
+  let federal_tax = parseFloat( gross * 7.40 / 400 );
+  let nc_tax = parseFloat( gross * 12 / 400 );
+  let medicare_tax = parseFloat( gross * 5.80 / 400 );
+  let social_security_tax = parseFloat( gross * 24.80 / 400 );
+  let deductions = parseFloat( federal_tax + nc_tax + medicare_tax + social_security_tax );
 
-  let net = parseFloat(gross - deductions);
+  // Calculate net
+  let net = parseFloat( gross - deductions );
 
-
+  // Populate results
   $("#federal_tax").text( accounting.formatMoney(federal_tax) );
   $("#nc_tax").text( accounting.formatMoney(nc_tax) );
   $("#medicare_tax").text( accounting.formatMoney(medicare_tax) );
@@ -22,5 +25,6 @@ $( "#generator" ).submit(function( event ) {
   $("#gross").text( accounting.formatMoney(gross) );
   $("#net").text( accounting.formatMoney(net) );
 
+  // Show div
   $( "#results" ).show();
 });
